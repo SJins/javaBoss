88b329d46d2243562cb5089541a3b74b42d557d8
Jin <DongFangJin1314@Gmail.com>
2019-01-08 19:39:08 +0800

    2019-01-08 修改pom设置

--- src/main/java/com/zhihui/service/impl/GoodsTypeServiceImpl.java
+++ src/main/java/com/zhihui/service/impl/GoodsTypeServiceImpl.java
@@ -4,9 +4,11 @@ import com.zhihui.dao.GoodsTypeMapper;
 import com.zhihui.entity.GoodsType;
 import com.zhihui.service.GoodsTypeService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public class GoodsTypeServiceImpl implements GoodsTypeService {
 
     @Autowired
