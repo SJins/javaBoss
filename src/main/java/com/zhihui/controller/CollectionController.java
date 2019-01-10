ebb088dcda96dccabde769591d3c43199cd48c05
彭智慧 <3350467026@qq.com>
2019-01-10 14:47:39 +0800

    收货详情

--- src/main/java/com/zhihui/controller/CollectionController.java
+++ src/main/java/com/zhihui/controller/CollectionController.java
@@ -3,6 +3,7 @@ package com.zhihui.controller;
 import com.zhihui.entity.Goods;
 import com.zhihui.service.CollectiongService;
 import com.zhihui.vo.ResultVo;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,10 +11,12 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
+
 public class CollectionController {
     @Autowired
     private CollectiongService collectiongService;
 
+    @ApiOperation(notes = "我的收藏接口",value = "商品收藏")
     @RequestMapping("findCollById.do")
     public ResultVo findCollById(Integer uid){
         List<Goods> list = collectiongService.findCollById(uid);
--- src/main/java/com/zhihui/controller/ReceivingController.java
+++ src/main/java/com/zhihui/controller/ReceivingController.java
@@ -2,6 +2,7 @@ package com.zhihui.controller;
 
 import com.zhihui.service.ReceivingService;
 import com.zhihui.vo.ResultVo;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 @CrossOrigin
 @RestController
 @RequestMapping("/receiving")
+@ApiOperation(notes = "我的收货地址接口", value = "收货地址")
 public class ReceivingController {
     @Autowired
     private ReceivingService receivingService;
