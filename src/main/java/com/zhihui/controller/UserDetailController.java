28b812695a3179f21d80a82e708699b37f5a73f1
ning <bear_rain@foxmail.com>
2019-01-10 21:30:40 +0800

    添加文件上传jar包 修改swagger页面访问链接 添加修改信息接口

--- src/main/java/com/zhihui/controller/UserDetailController.java
+++ src/main/java/com/zhihui/controller/UserDetailController.java
@@ -1,5 +1,6 @@
 package com.zhihui.controller;
 
+import com.zhihui.entity.UserDetail;
 import com.zhihui.service.UserDetailService;
 import com.zhihui.vo.ResultVo;
 import io.swagger.annotations.Api;
@@ -7,6 +8,12 @@ import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 @Api(produces = "用户详情类接口文档", value = "接口文档")
 @RestController
@@ -18,5 +25,30 @@ public class UserDetailController {
     @PostMapping("/user/myself.do")
     public ResultVo myself(String token) {
         return userDetailService.myselfMsg(token);
+    }
+
+    @ApiOperation(notes = "修改个人信息", value = "修改信息")
+    @PostMapping("/updateMsg.do")
+    public ResultVo updateMsg(String token, HttpServletRequest request, UserDetail userDetail) throws IOException {
+        String serverName = request.getSession().getServletContext().getRealPath("/");
+        File file = new File(serverName);
+        File parentFile = file.getParentFile();
+        String path = parentFile.getPath();
+        File upFile = new File(path, "/images");
+        String sqlpath = null;
+        String dirpath = "http://13.250.74.156:8080/images/";
+        if (!userDetail.getFile().isEmpty()) {
+            if (upFile.exists()) {
+                upFile.mkdirs();
+            }
+            SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+
+            String filename = sdf.format(new Date()) + userDetail.getFile().getOriginalFilename();
+
+            userDetail.getFile().transferTo(new File(dirpath, filename));
+            userDetail.setImg(filename);
+            return userDetailService.updateMsg(token, userDetail);
+        }
+        return ResultVo.setERROR();
     }
 }
--- src/main/java/com/zhihui/service/UserDetailService.java
+++ src/main/java/com/zhihui/service/UserDetailService.java
@@ -1,8 +1,11 @@
 package com.zhihui.service;
 
+import com.zhihui.entity.UserDetail;
 import com.zhihui.vo.ResultVo;
 
 public interface UserDetailService {
 
     ResultVo myselfMsg(String token);
+
+    ResultVo updateMsg(String token, UserDetail userDetail);
 }
