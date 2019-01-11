a06e12c24aba98e810a923c11bbd3d52d3bdf59c
彭智慧 <3350467026@qq.com>
2019-01-11 16:29:57 +0800

    修改请求方式

--- src/main/java/com/zhihui/controller/ReceivingController.java
+++ src/main/java/com/zhihui/controller/ReceivingController.java
@@ -5,10 +5,8 @@ import com.zhihui.service.ReceivingService;
 import com.zhihui.vo.ResultVo;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
 @CrossOrigin
 @RestController
 @RequestMapping("/receiving")
@@ -18,7 +16,7 @@ public class ReceivingController {
     private ReceivingService receivingService;
 
     @ApiOperation(notes = "我的收货地址接口", value = "我的地址")
-    @RequestMapping("findOneRec.do")
+    @GetMapping("findOneRec.do")
     public ResultVo findOneRec(Integer uid){
         return  receivingService.findOneRec(uid);
     }
